330c9e59f24a9146372c6f70f6e6b06c831e7778
arthur607 <arthurrodriguesg1@gmail.com>
2021-08-03 20:56:03 -0300

    exLambdaFinalizado

--- src/com/company/projects/exLambda/program.java
+++ src/com/company/projects/exLambda/program.java
@@ -4,22 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 
 public class program {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
         List<Employee> employees = new ArrayList<>();
 
 
         employees.add(new Employee("arthur", "arthur@gmail.com", 2300.0));
         employees.add(new Employee("fabio", "fabio@gmail.com", 3000.0));
-        employees.add(new Employee("bob", "bob@outlook.com", 5000.0));
-        employees.stream()
-                .filter(x -> x.getSalary() >= 4000.0 && x.getEmail().charAt(0) == 'b')
-                .map(x -> x.getSalary() * 10);
+        employees.add(new Employee("aaron", "bob@outlook.com", 5000.0));
+        employees.add(new Employee("bob", "bobs@outlook.com", 5500.0));
+
+        double sum =  employees.stream()
+               .filter(x -> x.getSalary() >= 4000.0 && x.getEmail().charAt(0) == 'b')
+               .map(p -> p.getSalary())     //buscando apenas o salário
+               .reduce(0.0, (x,y) -> x + y);
+
+      List<String> order = employees.stream()
+              .map(p -> p.getName())
+              .sorted()    //ordem alfabética
+              .collect(Collectors.toList());   //transformando novamente em lista
 
 
+        order.forEach(System.out :: println);
+
+        System.out.println("sum: "+sum);
 
     }
 
