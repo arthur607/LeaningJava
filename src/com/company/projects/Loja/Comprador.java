95cf68ad493000052b8a93396ac9279ee8cc4235
arthur607 <arthurrodriguesg1@gmail.com>
2021-08-05 22:35:37 -0300

    ajustes

--- src/com/company/projects/Loja/Comprador.java
+++ src/com/company/projects/Loja/Comprador.java
@@ -3,11 +3,24 @@ package com.company.projects.Loja;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+import java.util.Scanner;
+
 
 public class Comprador extends Pessoa {
 
-
+    Scanner sc = new Scanner(System.in);
     public Comprador(String nome, int idade, int cpf) {
         super(nome, idade, cpf);
+    }
+
+    public void comprarProduct(List<Produto> p){
+        Produto pr = new Produto();
+        System.out.println("digite o produto que deseja comprar:");
+        pr.setNome(sc.nextLine());
+        if (p.contains(pr.getNome())){
+        p.stream().map(produto -> produto.getStock() -1 );
+        }
+        p.forEach(System.out :: println);
     }
 }
--- src/com/company/projects/Loja/Produto.java
+++ src/com/company/projects/Loja/Produto.java
@@ -1,8 +1,12 @@
 package com.company.projects.Loja;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Produto {
     private String nome;
     private int quantity;
